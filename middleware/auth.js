907b5d37442b0e239d07959c4935008a5b406e0b
Shubham Kumar <98862179+shubham21155102@users.noreply.github.com>
2022-12-23 01:07:51 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,12 +5,17 @@ const auth=async (req,res,next)=>{
         const token=req.cookies.jwt;
         const verifyUser=jwt.verify(token,process.env.SECRET_KEY);
         console.log(verifyUser);
-        const user=await UserModel.findOne({_id:verifyUser._id});
+        const user=await UserModel.findOne({_id:verifyUser._id,"tokens:token":token});
+        if(!user){ throw new Error('User Not Found')};
+        req.token=token;
+        req.user=user;
+        req.userID=user._id;
         next();
     }
     catch(error)
     {
-        res.status(401).send(error);           
+        res.status(401).send("Unauthorized:No token provided.");  
+        console.log(error)         
     }
 }
 module.exports=auth;
