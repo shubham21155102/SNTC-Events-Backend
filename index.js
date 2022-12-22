9a95ac34dc94a106c25857e5b401287247efc831
Shubham Kumar <98862179+shubham21155102@users.noreply.github.com>
2022-12-22 17:25:36 +0530

    event routes has been added

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express=require('express');
 const env=require('dotenv').config();
 const app=express();
+const mongoose = require('mongoose');
+const cors=require('cors');
+app.use(cors());
+const cookieParser=require('cookie-parser');
+app.use(cookieParser());
+mongoose.set('strictQuery', true);
 //middlewares
 app.use(express.json());
 const port=process.env.PORT || 8000;
+//routes
+app.use('/api',require('./routes/auth'));
+app.use('/api',require('./routes/events'));
 //mongo db connection 
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
+
 mongoose
 .connect(process.env.DATABASE,{
  useNewUrlParser: true,
